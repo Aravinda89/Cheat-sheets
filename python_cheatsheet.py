c402fae425cb2c8a127241b9e4a0a84ebb3f71c6
Aravinda Gayan <aravindagayan@Aravindas-MacBook-Pro.local>
2019-03-23 11:52:26 +0800

    String,lists commands

--- python_cheatsheet.py
+++ python_cheatsheet.py
@@ -12,14 +12,37 @@ x/float(2)
 
 help(str)
 
-# String
+# Strings
 string1 = "ThisIsMyString"
 
 string1*2
 string1+'_NotYours'
 'M' in string1
 
+string1[4]
+string1[6:]
+
+string1.upper()
+string1.lower()
+string1.count('i')
+string1.lower().count('i')
+string1.replace('y','Y')
+string1.strip()
 
 
+# Lists
+a = 'three'
+b = 'four'
+my_list = ['one','two', a, b]
 
+my_list2 = [[1,2,3,4],[5,6,7,8]]
+
+my_list[-2]
+my_list2[-1]
+my_list[1:3]
+my_list[1:]
+my_list[:3]
+
+my_list[1][1]
+my_list2[1][2]
 
