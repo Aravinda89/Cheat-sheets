35514ae3a5bf41c59833823d6894bdb94c33b786
aravindagayan <1513223865@qq.com>
2019-03-27 20:57:16 +0800

    Create numpy array

---
+++ numpy_cheatsheet.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+a = np.array([1,2,3])
+b = np.array([(1.5,2,3), (4,5,6)])
+b = np.array([[1.5,2,3], [4,5,6]])
+c = np.array([[(1.5,2,3), (4,5,6)], [(3,2,1), (4,5,6)]], dtype = float)
+
