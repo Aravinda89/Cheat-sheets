09a6570441a80fae859e32a8f0474ae8e2e3957d
aravindagayan <1513223865@qq.com>
2019-03-27 21:10:18 +0800

    Numpy placeholders

--- numpy_cheatsheet.py
+++ numpy_cheatsheet.py
@@ -1,7 +1,18 @@
 import numpy as np
 
-a = np.array([1,2,3])
-b = np.array([(1.5,2,3), (4,5,6)])
-b = np.array([[1.5,2,3], [4,5,6]])
-c = np.array([[(1.5,2,3), (4,5,6)], [(3,2,1), (4,5,6)]], dtype = float)
+# Creating Arrays
+a = np.array([1, 2, 3])
+b = np.array([(1.5, 2, 3), (4, 5, 6)])
+b = np.array([[1.5, 2, 3], [4, 5, 6]])
+c = np.array([[(1.5, 2, 3), (4, 5, 6)], [(3, 2, 1), (4, 5, 6)]], dtype=float)
 
+# Placeholders
+zeros = np.zeros((2, 2))
+ones = np.ones((2, 2))
+d = np.arange(0, 51, 5)
+np.linspace(0, 21, 3)
+
+np.full((3,3),5)
+np.eye(3)
+np.random.random((5,5))
+np.empty((2,2))
