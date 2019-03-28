f1324451d3ed6027461d19beadb388cef39d11d6
aravindagayan <1513223865@qq.com>
2019-03-28 22:37:55 +0800

    Add more commands

--- numpy_cheatsheet.py
+++ numpy_cheatsheet.py
@@ -28,7 +28,7 @@ b.dtype
 b.dtype.name
 b.astype(int)
 
-#Array Mathmatics
+# Array Mathmatics
 a-b
 np.subtract(a,b)
 a+b
@@ -44,7 +44,77 @@ np.cos(b)
 np.log(b)
 a.dot(b.T)
 
-#Comparison
+# Comparison
 a == b
 a < 3
 np.array_equal(a, a.copy())
+
+# Aggregate function
+a.sum()
+a.min()
+b.max(axis=0)
+b.cumsum(axis=0)
+a.mean()
+np.std(b)
+
+# Copy arrays
+h = a.view()
+np.copy(a)
+g = a.copy()
+
+# Sorting
+a.sort()
+b.sort(axis=0)
+
+# Subsetting
+a[2]
+b[0, 1]
+
+# Slicing
+a[0: 2]
+b[:, 1]
+b[: 1]
+
+c[1, :, :]
+c[1, ...]
+
+a[: :-1] # Reverse array
+
+# Boolean Indexing
+b[b > 4]
+
+b[[0, 1, 1],[0, 1, 2]]
+b[[1, 0]][:, [2]]
+
+# Manipulation
+np.transpose(b)
+b.T
+
+b.ravel()
+b.reshape(3, -1)
+b.resize(3, 2)
+np.append(a, -2)
+np.insert(a, 1, 20)
+np.delete(a, [1])
+
+# Combining
+j = np.array([[3, 3]])
+np.concatenate((b, j), axis=0)
+
+k = np.array([0, 0])
+np.vstack((k, b))
+
+l = np.array([1, 1])
+
+np.r_[k, l]
+np.hstack((k, l))
+np.column_stack((k, l))
+np.c_[k, l]
+
+# Splitting
+np.hsplit(a, 2)
+np.vsplit(b, 3)
+
+
+
+
