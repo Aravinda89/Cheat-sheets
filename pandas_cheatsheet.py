98a143070f254d1ae5fa40deff5eb1385d26e7f4
aravindagayan <1513223865@qq.com>
2019-03-31 20:27:41 +0800

    stack unstack

--- pandas_cheatsheet.py
+++ pandas_cheatsheet.py
@@ -5,7 +5,12 @@ df = pd.DataFrame({'Date': ['2016-03-01', '2016-03-02', '2016-03-03'],
 
 # Rows into columns
 df1 = df.pivot(index='Date', columns='Type', values='Value')
-
 df2 = pd.pivot_table(df, index='Date', columns='Type', values='Value')
+
+# Stack/ Unstack
+df3 = pd.DataFrame({'0': [0.23, 0.18, 0.43], '1': [0.39, 0.23, 0.43]})
+df4 = df3.stack()
+df4.unstack()
+
 
 
