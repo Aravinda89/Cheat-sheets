061ee711224ee96fca97de6d5f088832efc08a13
aravindagayan <1513223865@qq.com>
2019-03-31 20:16:32 +0800

    pandas pivot

--- pandas_cheatsheet.py
+++ pandas_cheatsheet.py
@@ -1,1 +1,11 @@
-import pandas
+import pandas as pd
+
+df = pd.DataFrame({'Date': ['2016-03-01', '2016-03-02', '2016-03-03'],
+                   'Type': ['a', 'b', 'c'], 'Value': [11.432, 13.031, 20.234]})
+
+# Rows into columns
+df1 = df.pivot(index='Date', columns='Type', values='Value')
+
+df2 = pd.pivot_table(df, index='Date', columns='Type', values='Value')
+
+
